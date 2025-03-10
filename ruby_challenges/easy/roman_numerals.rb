71b95bb772d7b2c62489d4091b69f22b926b411d
Nora Vogt <noravogt@protonmail.com>
2025-03-10 13:27:16 -0400

    refactored solution

--- ruby_challenges/easy/roman_numerals.rb
+++ ruby_challenges/easy/roman_numerals.rb
@@ -30,127 +30,103 @@ EXAMPLES:
 2000=MM
 8=VIII
 
-DATA STRUCTURES:
+LOOKING FOR PATERN - UP TO 3000:
 
-roman numeral hash:
-I - 1
-V - 5
-X - 10
-L - 50
-C - 100
-D - 500
-M - 1000
+1-10
+I V X
 
-DECIMAL_TO_ROMAN = { 1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
-BRAINSTORM:
-
-1990 => MCMXC
-1000 M
-900 CM
-90 XC
-0 -
-
-2839 => MMDCCCXXXIX
-break into units - %
-2839
 2000 - MM
-800 - DCCC
-30 - XXX
-9 - IX
+800 - (hash[tens][1]) + (hash[tens] * (8-5)) +    DCCC
+30 -        XXX
+9 -         IX
 
-1003 => MIII
-1000
-000 skip
-00 skip
-3
+thousands: M 
+hundreds: C D M
+tens: X L C
+ones: I V X
 
-check length of number?
-if 4 digits
-- check leading digit (1, 2, 3)
-- concatenate that many 'M's
+1-3000: multiply number of Ms by first digit
+100-900: 
 
-elsif 3
-- check leading digit (0-9)
-d => 500
+[C D M], [X L C], [I V X]
+1-3: 3 of first digit
+4: first + second digit
+5: second digit
+6-8: second digit + increasing number of first digit
+9: first digit + third digit
 
- - if 900: CM
- - if 800: DCCC
- - if 700: DCC
- - if 600: DC
- - if 500: D
- - if 400: CD
+0 ones => [I V X]
+1 tens => [X L C]
+2 hundreds => [C D M]
+3 thousands => [M]
 
- if 1-3, 6-8: C * num
- - if 300: CCC
- - if 200: CC
- - if 100: C
+[2000, 800, 30, 9]
+2000
 
-elsif 2
-  - 90: XC
-  - 80: LXXX
-  - 70: LXX
-  - 60: LX
-  - 50: L
-  - 40: XL
-  - 30: XXX
-  - 20: XX
-  - 10: X
+base values - conversion hash
+1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
 
-20 => 'XX', 30 => 'XXX', 40 => 'XL', 50 => 'L', 60 => 'LX', 70 => 'LXX', 80 => 'LXXX', 90 => 'XC'
+find largest base
+number: 2839
+number: 839
 
-else (1 digit)
+DATA STRUCTURES:
+- conversion hash:
+
+
+
+
 
 ALGO
-# initialize
+# initialize - takes an integer
+- set @number to integer
 
 # to_roman
+- set roman_string = ''
+- set remainder = @number
 
-# expand
-  # iterate through array of chars in string, with index
-  # create new string slice, get char at current index, replace subsequent chars with zero
-  # add string to array
+loop while remainder > 0
+  - get largest base number in conversion hash
+    - iterate through hash conversion values keys - #find
+      - select the first value that is less than or equal to current remainder
+  - get roman numeral associated with that key in hash, set to current_numeral
+  - calculate quotient and remainder of dividing that number - [Q, R]
+  - numeral * quotient, add to result string
+  - set remainder to R
+
 =end
 
 
 class RomanNumeral
   DECIMAL_TO_ROMAN = { 
-    1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV',
-    5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX',
-    10 => 'X', 20 => 'XX', 30 => 'XXX', 40 => 'XL', 
-    50 => 'L', 60 => 'LX', 70 => 'LXX', 80 => 'LXXX', 90 => 'XC', 100 => 'C',
-    200 => 'CC', 300 => 'CCC', 400 => 'CD', 500 => 'D', 600 => 'DC', 
-    700 => 'DCC', 800 => 'DCCC', 900 => 'CM', 1000 => 'M'}
+    1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 
+    100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 
+    10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 
+    1 => 'I'
+  }
 
   def initialize(number)
     @number = number
   end
 
   def to_roman
-    expanded_number = expand
-    roman = ''
-    expanded_number.each do |place_value|
-      first_digit = place_value[0].to_i
-      if place_value.size == 4
-        roman << ('M' * first_digit)
-      elsif
-        roman << DECIMAL_TO_ROMAN[place_value.to_i]
-      end
-    end
-    roman
-  end
+    roman_numeral = ''
+    remainder = @number
 
-  def expand
-    result = []
-    number_string = @number.to_s
-    0.upto(number_string.size - 1) do |index|
-      next if number_string[index] == '0'
-      result << number_string[index] + ('0' * (number_string.size - index - 1))
+    while remainder > 0
+      biggest_divisor = DECIMAL_TO_ROMAN.keys.find { |n| n <= remainder }
+      current_numeral = DECIMAL_TO_ROMAN[biggest_divisor]
+      quotient, remainder = remainder.divmod(biggest_divisor)
+      roman_numeral << (current_numeral * quotient)
     end
-    result
+    roman_numeral
   end
 end
 
 
 
 # 2839 => MMDCCCXXXIX
-# RomanNumerals.new(2839).to_roman
+# RomanNumeral.new(2839).to_roman == 'MMDCCCXXXIX'
+RomanNumeral.new(1).to_roman == 'I'
+# RomanNumeral.new(4).to_roman == 'IV'
+
